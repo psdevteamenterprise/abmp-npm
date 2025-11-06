ecfb0a3ce2c1d99d0e822942b7065a6c87cafd0a
dianasaker <dianasa@wix.com>
2025-11-06 09:57:40 +0200

    update code

--- pages/SaveAlerts.js
+++ pages/SaveAlerts.js
@@ -1,11 +1,10 @@
 const { lightbox } = require('@wix/site-window');
 
-const { ABMP_MEMBERS_HOME_URL } = require('../public/consts');
-
 function saveAlertsOnReady({ $w: _$w }) {
+  const receivedData = lightbox.getContext();
   _$w('#closeButton').onClick(async () => await lightbox.close());
   _$w('#cancelButton').onClick(async () => await lightbox.close());
-  _$w('#leaveButton').link = ABMP_MEMBERS_HOME_URL;
+  _$w('#leaveButton').link = receivedData?.membersExternalPortalUrl;
   _$w('#leaveButton').target = '_blank';
 }
 
