21564ef8b36fa3a14641fe5108c9569e7ed49283
Besan-Darawsheh <69515923+Besan-Darawsheh@users.noreply.github.com>
2026-03-16 11:46:15 +0200

    chore: implement createMissingUrls dev only script

--- backend/dev-only-methods.js
+++ backend/dev-only-methods.js
@@ -1,6 +1,7 @@
 const { COLLECTIONS } = require('../public/consts');
 
 const { ensureUniqueUrlsInBatch } = require('./daily-pull/bulk-process-methods');
+const { ensureUniqueUrl } = require('./daily-pull/process-member-methods');
 const { wixData } = require('./elevated-modules');
 const { bulkSaveMembers } = require('./members-data-methods');
 const { queryAllItems } = require('./utils');
@@ -27,4 +28,31 @@ async function copyContactIdToWixMemberId() {
   return await bulkSaveMembers(updatedMembers, COLLECTIONS.MEMBERS_DATA);
 }
 
-module.exports = { deduplicateURls, copyContactIdToWixMemberId };
+async function createMissingUrls() {
+  const query = wixData.query(COLLECTIONS.MEMBERS_DATA).isEmpty('url');
+  const members = await queryAllItems(query);
+  console.log(
+    'membersWithoutUrls info',
+    JSON.stringify({
+      count: members.length,
+      membersIds: members.map(m => m.memberId),
+    })
+  );
+
+  const membersWithGeneratedUrlsPromises = members.map(async member => ({
+    ...member,
+    url: await ensureUniqueUrl({
+      url: member.url,
+      memberId: member.memberId,
+      fullName: member.fullName,
+    }),
+  }));
+  const membersWithGeneratedUrls = await Promise.all(membersWithGeneratedUrlsPromises);
+  //recheck urls in same batch to avoid duplicates
+  const uniqueUrlsUpdatedMembers = await ensureUniqueUrlsInBatch(membersWithGeneratedUrls);
+  const urls = uniqueUrlsUpdatedMembers.map(m => m.url).filter(Boolean);
+  console.log('unique urls', [...new Set(urls)]);
+  return await bulkSaveMembers(uniqueUrlsUpdatedMembers, COLLECTIONS.MEMBERS_DATA);
+}
+
+module.exports = { deduplicateURls, copyContactIdToWixMemberId, createMissingUrls };
